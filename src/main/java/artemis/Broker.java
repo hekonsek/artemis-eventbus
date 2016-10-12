7eee5fde1298e12ab49cbdc168f2829f11b3224b
Henryk Konsek <hekonsek@gmail.com>
2016-10-12 22:54:46 +0200

    Added Camel magic.

--- src/main/java/artemis/Broker.java
+++ src/main/java/artemis/Broker.java
@@ -2,6 +2,7 @@ package artemis;
 
 import org.apache.activemq.artemis.core.config.Configuration;
 import org.apache.activemq.artemis.core.config.CoreQueueConfiguration;
+import org.apache.activemq.artemis.core.settings.impl.AddressSettings;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
@@ -21,8 +22,7 @@ public class Broker {
             public void customize(Configuration configuration) {
                 try {
                     configuration.addAcceptorConfiguration("amqp-acceptor", "tcp://localhost:5672?protocols=AMQP");
-//                    configuration.addAddressesSetting("#", new AddressSettings().setAutoCreateJmsQueues(true).setAutoCreateJmsTopics(true));
-                    configuration.addQueueConfiguration(new CoreQueueConfiguration().setAddress("foo").setName("foo"));
+                    configuration.addAddressesSetting("#", new AddressSettings().setAutoCreateJmsQueues(true));
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
@@ -36,7 +36,7 @@ public class Broker {
             @Override
             public void configure() throws Exception {
                 getContext().addComponent("amqp", amqpComponent("amqp://localhost:5672"));
-                from("amqp:foo").log("hello!");
+                from("amqp:jms.queue.foo").log("hello!");
             }
         };
     }
@@ -45,7 +45,7 @@ public class Broker {
         System.setProperty("spring.artemis.mode", "native");
         ProducerTemplate producer = new SpringApplicationBuilder(Broker.class).run(args).getBean(ProducerTemplate.class);
 
-        Object response = producer.requestBody("amqp:foo", "xxx");
+        Object response = producer.requestBody("amqp:jms.queue.foo", "xxx");
         System.out.println(response);
     }
 
